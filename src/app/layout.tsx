3eef890fa11da9c1d9d7d8c0d51dc8124bd42b41
Dylan Hecht <dylanhecht@mac.mynetworksettings.com>
2025-11-03 13:07:17 -0500

    Trigger redeploy with environment variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
+// Trigger redeploy with environment variables
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
